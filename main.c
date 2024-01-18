509dfdd416decd89dbe1580d2edd646a13d24e13
osmonduu <osmondwa@gmail.com>
2024-01-17 19:23:21 -0500

    created main skeleton for main loop

--- main.c
+++ main.c
@@ -117,6 +117,70 @@ int main(int argc, const char* argv[])
     // since exactly one condition flag should be set at any given time, set the z flag
     reg[R_COND] = FL_ZRO;
 
+    // set the PC to starting position
+    // 0x3000 is the default 
+    enum { PC_START = 0x3000 };
+    reg[R_PC] = PC_START;
+
+    int running = 1;
+    while (running) 
+    {
+        /* FETCH */
+        uint16_t instr = mem_read(reg[R_PC]++);
+        uint16_t op = instr >> 12;
+
+        switch (op)
+        {
+            case OP_ADD:
+
+                break;
+            case OP_AND:
+
+                break;
+            case OP_NOT:
+
+                break;
+            case OP_BR:
+
+                break;
+            case OP_JMP:
+
+                break;
+            case OP_JSR:
+
+                break;
+            case OP_LD:
+
+                break;
+            case OP_LDI:
+
+                break;
+            case OP_LDR:
+
+                break;
+            case OP_LEA:
+
+                break;
+            case OP_ST:
+
+                break;
+            case OP_STI:
+
+                break;
+            case OP_STR:
+
+                break;
+            case OP_TRAP:
+
+                break;
+            case OP_RES:
+            case OP_RTI:
+            default:
+
+                break;
+            
+        }
+    }
     /* Shutdown */
     restore_input_buffering(); 
 }
